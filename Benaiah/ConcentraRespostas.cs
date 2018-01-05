17334c3c83099b47c4dc36c95245a7f2de56f86c
MURILO FUJITA <murilo.fujita@fatec.sp.gov.br>
2018-01-05 09:39:02 -0200

    Armazenamento das respostas em listas (provisório)

---
+++ Benaiah/ConcentraRespostas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benaiah
+{
+    class ConcentraRespostas
+    {
+        public void GeraRelatorio(List<ListaDeRespostas> todasRespostas)
+        {
+            FileStream fs = new FileStream("relatorio.html", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("<html>");
+            sw.WriteLine("<body>");
+            sw.WriteLine("<meta charset=utf8>");
+            sw.WriteLine("<table border=1>");
+            foreach (var item in todasRespostas)
+            {
+                sw.WriteLine("<tr><td>" + item.nome + "</td> <td>" + item.setor + "</td> <td>" + item.resposta + "</td> </tr>");
+            }
+            sw.WriteLine("</table>");
+            sw.WriteLine("</body>");
+            sw.WriteLine("</html>");
+            sw.Close();
+        }
+    }
+}
---
+++ Benaiah/ListaDeRespostas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benaiah
+{
+    public class ListaDeRespostas
+    {
+        public string nome;
+        public string setor;
+        public string resposta;
+
+        public ListaDeRespostas(string nomeFunc, string setorFunc, string respostaFunc)
+        {
+            nome = nomeFunc;
+            setor = setorFunc;
+            resposta = respostaFunc;
+        }
+    }
+}
